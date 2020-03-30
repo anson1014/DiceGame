f83e34108f6d28543e218954b31904ec19618a23
anson1014 <anson1014@hotmail.com>
2020-03-30 16:18:59 -0700

    Added dice roll random function, roll dice AE listener, and DOM css manipulation

--- starter/app.js
+++ starter/app.js
@@ -8,3 +8,73 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// CSS ID: current-1
+
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// alt way of altering text
+// var x = document.querySelector('#score-0').textContent;
+// getter
+
+// document.querySelector('.dice').style.display = 'none';
+// CSS CLASS: dice
+
+// function btn() {
+//     //
+// }
+
+// document.querySelector('.btn-roll').addEventListener('click', btn);
+// // btn is the callback function, ie: not called by us
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //called an anonymous function, does smt here
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score IF the rolled number is not 1
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        roundScore = 0;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        activePlayer === 0? activePlayer = 1 : activePlayer = 0;
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+
+        // document.querySelector('.player-1-panel').classList.toggle('active');
+        // document.querySelector('.player-0-panel').classList.toggle('active');
+        // ALTERNATIVE WAY USING TOGGLE
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add CURRENT score to GLOBAL score
+
+
+    // Update the UI
+
+
+    // Check if the player won the game
+});
