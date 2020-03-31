34e7a11acc092591aeb245e461cca4f01909169f
anson1014 <anson1014@hotmail.com>
2020-03-30 17:43:48 -0700

    added finishing touches

--- starter/app.js
+++ starter/app.js
@@ -1,13 +1,15 @@
 /*
 GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+
 
 */
+
+alert("HOW TO PLAY:\nEach turn, a player rolls a dice as many times as " + 
+"he wishes. Each result get added to his ROUND score " + "BUT, if the player rolls a 1, all his ROUND score gets lost. " +
+"After that, it's the next player's turn. The player can choose to 'Hold', which means that his ROUND score gets added to his GAME score. " + 
+"After that, it's the next player's turn. The first player to reach 50 points on GAME score wins the game.\n\n" + 
+"NOTE: IF a player rolls two 6s in a row, then his round score is set to 0");
 
 var scores, roundScore, activePlayer, gamePlaying, prevRoll;
 
@@ -83,7 +85,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
         var input = document.querySelector('.final-score').value;
 
-        var winVal = (input)? input : 100;
+        var winVal = (input)? input : 50;
 
         // Check if the player won the game
         if (scores[activePlayer] >= winVal) {
